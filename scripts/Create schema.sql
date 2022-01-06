683a7a155822e20f1c7ce8e35a059677165e52b0
Jakob Olsson <jakolss@kth.se>
2022-01-06 14:26:43 +0100

    Added caps in create schema

--- scripts/Create schema.sql
+++ scripts/Create schema.sql
@@ -1,273 +1,273 @@
-create type person_type as enum ('instructor', 'student', 'parent');
-create type lesson_type as enum ('ensamble', 'group_lesson', 'individual_lesson');
+CREATE type person_type AS enum ('instructor', 'student', 'parent');
+CREATE type lesson_type AS enum ('ensamble', 'group_lesson', 'individual_lesson');
 
-create table address
+CREATE TABLE address
 (
-    address_id       serial
-        constraint address_pkey
-            primary key,
-    country          varchar(100) not null,
-    city             varchar(100) not null,
-    street           varchar(100) not null,
-    street_number    integer not null,
-    zip              char(10) not null,
-    apartment_number varchar(100)
+    address_id       SERIAL
+        CONSTRAINT address_pkey
+            PRIMARY KEY,
+    country          VARCHAR(100) NOT NULL,
+    city             VARCHAR(100) NOT NULL,
+    street           VARCHAR(100) NOT NULL,
+    street_number    INTEGER NOT NULL,
+    zip              CHAR(10) NOT NULL,
+    apartment_number VARCHAR(100)
 );
 
-create table person
+CREATE TABLE person
 (
-    person_id serial
-        constraint person_pkey
-            primary key,
-    pnr       char(12) unique not null,
-    name      varchar(100) not null,
-    age       integer not null,
-    person_type      person_type not null
+    person_id SERIAL
+        CONSTRAINT person_pkey
+            PRIMARY KEY,
+    pnr       CHAR(12) UNIQUE NOT NULL,
+    name      VARCHAR(100) NOT NULL,
+    age       INTEGER NOT NULL,
+    person_type      person_type NOT NULL
 );
 
-create table address_person
+CREATE TABLE address_person
 (
-    person_id  integer
-        constraint address_person_pkey
-            primary key
-        constraint address_person_person_id_fkey
-            references person
-            on delete cascade,
-    address_id integer not null
-        constraint address_person_address_id_fkey
-            references address
-            on delete set null
+    person_id  INTEGER
+        CONSTRAINT address_person_pkey
+            PRIMARY KEY
+        CONSTRAINT address_person_person_id_fkey
+            REFERENCES person
+            ON DELETE CASCADE,
+    address_id INTEGER NOT NULL
+        CONSTRAINT address_person_address_id_fkey
+            REFERENCES address
+            ON DELETE SET NULL
 );
 
-create table contact_details
+CREATE TABLE contact_details
 (
-    contact_details_id serial
-        constraint contact_details_pkey
-            primary key,
-    phone              char(10) not null,
-    email              varchar(100) not null
+    contact_details_id SERIAL
+        CONSTRAINT contact_details_pkey
+            PRIMARY KEY,
+    phone              CHAR(10) NOT NULL,
+    email              VARCHAR(100) NOT NULL
 );
 
-create table instructor
+CREATE TABLE instructor
 (
-    instructor_id       serial
-        constraint instructor_pkey
-            primary key,
-    person_id           integer unique not null
-        constraint instructor_person_id_fkey
-            references person
-            on delete cascade,
-    contact_details_id  integer unique not null
-        constraint instructor_contact_details_id_fkey
-            references contact_details
-            on delete set null,
-    can_teach_ensambles boolean not null
+    instructor_id       SERIAL
+        CONSTRAINT instructor_pkey
+            PRIMARY KEY,
+    person_id           INTEGER UNIQUE NOT NULL
+        CONSTRAINT instructor_person_id_fkey
+            REFERENCES person
+            ON DELETE CASCADE,
+    contact_details_id  INTEGER UNIQUE NOT NULL
+        CONSTRAINT instructor_contact_details_id_fkey
+            REFERENCES contact_details
+            ON DELETE SET NULL,
+    can_teach_ensambles boolean NOT NULL
 );
 
-create table instrument_taught
+CREATE TABLE instrument_taught
 (
-    instrument_type varchar(100),
-    instructor_id   integer
-    constraint instrument_taught_instructor_id_fkey
-        references instructor
-        on delete cascade,
-    constraint instrument_taught_pkey
-        primary key (instrument_type, instructor_id)
+    instrument_type VARCHAR(100),
+    instructor_id   INTEGER
+    CONSTRAINT instrument_taught_instructor_id_fkey
+        REFERENCES instructor
+        ON DELETE CASCADE,
+    CONSTRAINT instrument_taught_pkey
+        PRIMARY KEY (instrument_type, instructor_id)
 );
 
-create table timeslot
+CREATE TABLE timeslot
 (
-    timeslot_id     serial
-        constraint timeslot_pkey
-            primary key,
-    start_time      timestamp not null,
-    end_time        timestamp not null,
-    instrument_type varchar(100)
+    timeslot_id     SERIAL
+        CONSTRAINT timeslot_pkey
+            PRIMARY KEY,
+    start_time      TIMESTAMP NOT NULL,
+    end_time        TIMESTAMP NOT NULL,
+    instrument_type VARCHAR(100)
 );
 
-create table instructor_timeslot
+CREATE TABLE instructor_timeslot
 (
-    instructor_id integer
-        constraint instructor_timeslot_instructor_id_fkey
-            references instructor
-            on delete cascade,
-    timeslot_id   integer
-        constraint instructor_timeslot_timeslot_id_fkey
-            references timeslot
-            on delete cascade,
-    constraint instructor_timeslot_pkey
-        primary key (instructor_id, timeslot_id)
+    instructor_id INTEGER
+        CONSTRAINT instructor_timeslot_instructor_id_fkey
+            REFERENCES instructor
+            ON DELETE CASCADE,
+    timeslot_id   INTEGER
+        CONSTRAINT instructor_timeslot_timeslot_id_fkey
+            REFERENCES timeslot
+            ON DELETE CASCADE,
+    CONSTRAINT instructor_timeslot_pkey
+        PRIMARY KEY (instructor_id, timeslot_id)
 );
 
-create table student
+CREATE TABLE student
 (
-    student_id      serial
-        constraint student_pkey
-            primary key,
-    person_id       integer unique not null
-        constraint student_person_id_fkey
-            references person
-            on delete cascade,
-    contact_details integer unique not null
-        constraint student_contact_details_fkey
-            references contact_details
-            on delete set null,
-    parent_contact  integer not null
-        constraint student_parent_contact_fkey
-            references contact_details
-            on delete set null,
-    parent_id       integer
-        constraint student_person_person_id_fk
-            references person
-            on delete set null
+    student_id      SERIAL
+        CONSTRAINT student_pkey
+            PRIMARY KEY,
+    person_id       INTEGER UNIQUE NOT NULL
+        CONSTRAINT student_person_id_fkey
+            REFERENCES person
+            ON DELETE CASCADE,
+    contact_details INTEGER UNIQUE NOT NULL
+        CONSTRAINT student_contact_details_fkey
+            REFERENCES contact_details
+            ON DELETE SET NULL,
+    parent_contact  INTEGER NOT NULL
+        CONSTRAINT student_parent_contact_fkey
+            REFERENCES contact_details
+            ON DELETE SET NULL,
+    parent_id       INTEGER
+        CONSTRAINT student_person_person_id_fk
+            REFERENCES person
+            ON DELETE SET NULL
 );
 
-create table sibling
+CREATE TABLE sibling
 (
-    sibling_id integer
-        constraint sibling_sibling_id_fkey
-            references student
-            on delete cascade,
-    student_id integer
-        constraint sibling_student_id_fkey
-            references student
-            on delete cascade,
-    constraint sibling_pkey
-        primary key (sibling_id, student_id)
+    sibling_id INTEGER
+        CONSTRAINT sibling_sibling_id_fkey
+            REFERENCES student
+            ON DELETE CASCADE,
+    student_id INTEGER
+        CONSTRAINT sibling_student_id_fkey
+            REFERENCES student
+            ON DELETE CASCADE,
+    CONSTRAINT sibling_pkey
+        PRIMARY KEY (sibling_id, student_id)
 );
 
-create table instrument
+CREATE TABLE instrument
 (
-    instrument_id serial
-        constraint instrument_pkey
-            primary key,
-    type          varchar(100) not null,
-    is_rented     boolean not null,
-    brand         varchar(100) not null,
-    sn            varchar(100) unique not null,
-    price         integer not null
+    instrument_id SERIAL
+        CONSTRAINT instrument_pkey
+            PRIMARY KEY,
+    type          VARCHAR(100) NOT NULL,
+    is_rented     boolean NOT NULL,
+    brand         VARCHAR(100) NOT NULL,
+    sn            VARCHAR(100) UNIQUE NOT NULL,
+    price         INTEGER NOT NULL
 );
 
-create table rental
+CREATE TABLE rental
 (
-    rental_id     serial
-        constraint rental_pkey
-            primary key,
-    student_id    integer not null
-        constraint rental_student_id_fkey
-            references student
-            on delete cascade,
-    instrument_id integer not null
-        constraint rental_instrument_id_fkey
-            references instrument
-            on delete cascade,
-    start_time    timestamp not null,
-    end_time      timestamp not null,
+    rental_id     SERIAL
+        CONSTRAINT rental_pkey
+            PRIMARY KEY,
+    student_id    INTEGER NOT NULL
+        CONSTRAINT rental_student_id_fkey
+            REFERENCES student
+            ON DELETE CASCADE,
+    instrument_id INTEGER NOT NULL
+        CONSTRAINT rental_instrument_id_fkey
+            REFERENCES instrument
+            ON DELETE CASCADE,
+    start_time    TIMESTAMP NOT NULL,
+    end_time      TIMESTAMP NOT NULL,
     with_delivery boolean,
-    price         integer not null
+    price         INTEGER NOT NULL
 );
 
-create table skill
+CREATE TABLE skill
 (
-    skill_id        serial
-        constraint skill_pkey
-            primary key,
-    instrument_type varchar(100) not null,
-    level           integer not null
+    skill_id        SERIAL
+        CONSTRAINT skill_pkey
+            PRIMARY KEY,
+    instrument_type VARCHAR(100) NOT NULL,
+    level           INTEGER NOT NULL
 );
 
-create table student_skill
+CREATE TABLE student_skill
 (
-    student_id integer
-        constraint student_skill_student_id_fkey
-            references student
-            on delete cascade,
-    skill_id   integer
-        constraint student_skill_skill_id_fkey
-            references skill
-            on delete cascade,
-    constraint student_skill_pkey
-        primary key (student_id, skill_id)
+    student_id INTEGER
+        CONSTRAINT student_skill_student_id_fkey
+            REFERENCES student
+            ON DELETE CASCADE,
+    skill_id   INTEGER
+        CONSTRAINT student_skill_skill_id_fkey
+            REFERENCES skill
+            ON DELETE CASCADE,
+    CONSTRAINT student_skill_pkey
+        PRIMARY KEY (student_id, skill_id)
 );
 
-create table music_lesson
+CREATE TABLE music_lesson
 (
-    music_lesson_id serial
-        constraint music_lesson_pkey
-            primary key,
-    instructor_id   integer
-        constraint music_lesson_instructor_id_fkey
-            references instructor
-            on delete set null,
-    executed        boolean not null,
-    lesson_type     lesson_type not null,
-    appointed_time  timestamp not null
+    music_lesson_id SERIAL
+        CONSTRAINT music_lesson_pkey
+            PRIMARY KEY,
+    instructor_id   INTEGER
+        CONSTRAINT music_lesson_instructor_id_fkey
+            REFERENCES instructor
+            ON DELETE SET NULL,
+    executed        boolean NOT NULL,
+    lesson_type     lesson_type NOT NULL,
+    appointed_time  TIMESTAMP NOT NULL
 );
 
-create table student_music_lesson
+CREATE TABLE student_music_lesson
 (
-    music_lesson_id integer
-        constraint student_music_lesson_music_lesson_id_fkey
-            references music_lesson
-            on delete cascade,
-    student_id      integer
-        constraint student_music_lesson_student_id_fkey
-            references student
-            on delete cascade,
-    constraint student_music_lesson_pkey
-        primary key (music_lesson_id, student_id)
+    music_lesson_id INTEGER
+        CONSTRAINT student_music_lesson_music_lesson_id_fkey
+            REFERENCES music_lesson
+            ON DELETE CASCADE,
+    student_id      INTEGER
+        CONSTRAINT student_music_lesson_student_id_fkey
+            REFERENCES student
+            ON DELETE CASCADE,
+    CONSTRAINT student_music_lesson_pkey
+        PRIMARY KEY (music_lesson_id, student_id)
 );
 
-create table group_lesson
+CREATE TABLE group_lesson
 (
-    music_lesson_id integer
-        constraint group_lesson_pkey
-            primary key
-        constraint group_lesson_music_lesson_id_fkey
-            references music_lesson
-            on delete cascade,
-    skill_id        integer not null
-        constraint group_lesson_skill_id_fkey
-            references skill
-            on delete set null,
-    min             integer not null,
-    max             integer not null,
-    instrument      varchar(100) not null
+    music_lesson_id INTEGER
+        CONSTRAINT group_lesson_pkey
+            PRIMARY KEY
+        CONSTRAINT group_lesson_music_lesson_id_fkey
+            REFERENCES music_lesson
+            ON DELETE CASCADE,
+    skill_id        INTEGER NOT NULL
+        CONSTRAINT group_lesson_skill_id_fkey
+            REFERENCES skill
+            ON DELETE SET NULL,
+    min             INTEGER NOT NULL,
+    max             INTEGER NOT NULL,
+    instrument      VARCHAR(100) NOT NULL
 );
 
-create table individual_lesson
+CREATE TABLE individual_lesson
 (
-    music_lesson_id integer
-        constraint individual_lesson_pkey
-            primary key
-        constraint individual_lesson_music_lesson_id_fkey
-            references music_lesson
-            on delete cascade,
-    skill_id        integer not null
-        constraint individual_lesson_skill_id_fkey
-            references skill
-            on delete set null,
-    instrument      varchar(100) not null
+    music_lesson_id INTEGER
+        CONSTRAINT individual_lesson_pkey
+            PRIMARY KEY
+        CONSTRAINT individual_lesson_music_lesson_id_fkey
+            REFERENCES music_lesson
+            ON DELETE CASCADE,
+    skill_id        INTEGER NOT NULL
+        CONSTRAINT individual_lesson_skill_id_fkey
+            REFERENCES skill
+            ON DELETE SET NULL,
+    instrument      VARCHAR(100) NOT NULL
 );
 
-create table ensamble
+CREATE TABLE ensamble
 (
-    music_lesson_id integer
-        constraint ensamble_pkey
-            primary key
-        constraint ensamble_music_lesson_id_fkey
-            references music_lesson
-            on delete cascade,
-    target_genre    varchar(100) not null,
-    min             integer not null,
-    max             integer not null
+    music_lesson_id INTEGER
+        CONSTRAINT ensamble_pkey
+            PRIMARY KEY
+        CONSTRAINT ensamble_music_lesson_id_fkey
+            REFERENCES music_lesson
+            ON DELETE CASCADE,
+    target_genre    VARCHAR(100) NOT NULL,
+    min             INTEGER NOT NULL,
+    max             INTEGER NOT NULL
 );
 
-create table lesson_price_scheme
+CREATE TABLE lesson_price_scheme
 (
-    lesson_type integer,
-    skill_level integer,
-    value       integer not null,
-    constraint lesson_price_scheme_pk
-        primary key (lesson_type, skill_level)
+    lesson_type INTEGER,
+    skill_level INTEGER,
+    value       INTEGER NOT NULL,
+    CONSTRAINT lesson_price_scheme_pk
+        PRIMARY KEY (lesson_type, skill_level)
 );
