4c2a3c5e247931802b9820894d646d48df94b43f
Samuel Ferrara <55464329+ferrara-dev@users.noreply.github.com>
2022-01-07 22:02:33 +0100

    Update query1.sql

--- scripts/queries/query1.sql
+++ scripts/queries/query1.sql
@@ -5,29 +5,29 @@
    expected to be performed a few times per week. */
    
 WITH given_lessons_in_selected_year as (
-	SELECT *
-	FROM music_lesson
-	WHERE extract (year FROM appointed_time) = 2021 AND executed = true
+    SELECT *
+    FROM music_lesson
+    WHERE extract (year FROM appointed_time) = 2021 AND executed = true
 ),
-lessons_per_month as (
-	SELECT COUNT(extract (month FROM appointed_time)), extract (month FROM appointed_time) as month, lesson_type
-	FROM given_lessons_in_selected_year
-	GROUP BY month, lesson_type
-),
-lessons_pivoted as (
-	SELECT month,
-		   round(avg(CASE WHEN lesson_type = 'individual_lesson' THEN count END), 0) AS individual,
-		   round(avg(CASE WHEN lesson_type = 'group_lesson' THEN count END), 0) AS group,
-	       round(avg(CASE WHEN lesson_type = 'ensamble' THEN count END), 0) AS ensamble
-	FROM lessons_per_month
-	GROUP BY month
-	ORDER BY month
-),
-total_per_month as (
-	SELECT SUM(count) as total, month
-	FROM lessons_per_month
-	GROUP BY month
-)
+     lessons_per_month as (
+         SELECT COUNT(extract (month FROM appointed_time)) as count, extract (month FROM appointed_time) as month, lesson_type
+         FROM given_lessons_in_selected_year
+         GROUP BY month, lesson_type
+     ),
+     lessons_pivoted as (
+         SELECT month,
+                round(avg(COALESCE(CASE WHEN lesson_type = 'individual_lesson' THEN count END,0)), 0) AS individual,
+                round(avg(COALESCE(CASE WHEN lesson_type = 'group_lesson' THEN count END,0)), 0) AS group,
+                round(avg(COALESCE(CASE WHEN lesson_type = 'ensamble' THEN count END,0)), 0) AS ensamble
+         FROM lessons_per_month
+         GROUP BY month
+         ORDER BY month
+     ),
+     total_per_month as (
+         SELECT SUM(count) as total, month
+         FROM lessons_per_month
+         GROUP BY month
+     )
 
 SELECT lessons_pivoted.month, lessons_pivoted.individual, lessons_pivoted.group, lessons_pivoted.ensamble, total
 FROM lessons_pivoted JOIN total_per_month ON lessons_pivoted.month = total_per_month.month
